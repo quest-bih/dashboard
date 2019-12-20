c1e9c79b3668dac5d248487af460a513cb580a52
Nico Riedel <nico.riedel@bihealth.de>
2019-12-20 11:08:10 +0100

    Open Data & Code Script

---
+++ Open_Data/Open_Data.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+#conversion of PDFs
+pdf_main_folder <- "C:/Datenablage/charite_dashboard/PDFs"
+txt_main_folder <- "C:/Datenablage/charite_dashboard/PDFs_to_text"
+
+pdf_folders <- list.dirs(pdf_main_folder)[-1]
+
+for(pdf_folder in pdf_folders)
+{
+  city <- pdf_folder %>% str_split("/") %>% map_chr(tail, 1)
+  txt_folder <- paste0(txt_main_folder, "/", city)
+  if(!dir.exists(txt_folder)) {
+    dir.create(txt_folder)
+  }
+  conversion_success <- oddpub::pdf_convert(pdf_folder, txt_folder)
+}
+
+
+#run ODDPub
+txt_folders <- list.dirs(txt_main_folder)[-1]
+
+for(txt_folder in txt_folders)
+{
+  year <- txt_folder %>% str_split("/") %>% map_chr(last)
+  print(year)
+
+  pdf_text_corpus <- oddpub::pdf_load(paste0(txt_folder, "/"))
+  oddpub_results <- oddpub::open_data_search(pdf_text_corpus)
+  oddpub_results["year"] <-
+  write_csv(oddpub_results, paste0("./results/Open_Data_", year, ".csv"))
+}
