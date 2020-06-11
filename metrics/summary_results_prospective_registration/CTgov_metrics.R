33dbfedabbc74d5f5bdad540edecff2d6d777f12
Nico RIedel <nico.riedel@bihealth.de>
2020-06-11 17:45:46 +0200

    Updated CTgov metrics calculation
    Now searches trials automatically by sponsor/PI name instead of using the old list of manually verified trials. This allows automatic updating of metric and does not produce false positives according to a manual check.

--- metrics/summary_results_prospective_registration/CTgov_metrics.R
+++ metrics/summary_results_prospective_registration/CTgov_metrics.R
@@ -6,7 +6,7 @@ library(lubridate)
 #----------------------------------------------------------------------------------------------------------------------
 
 #the AACT dataset has to be downloaded first from https://aact.ctti-clinicaltrials.org/pipe_files
-AACT_folder <- "C:/Datenablage/AACT/AACT dataset Nov 2019/" #insert the AACT download folder here
+AACT_folder <- "C:/Datenablage/AACT/AACT dataset 20200603/" #insert the AACT download folder here
 
 #AACT filenames that we need to load
 AACT_dataset_names <- c("studies", "overall_officials", "sponsors", "responsible_parties",
@@ -25,15 +25,65 @@ AACT_datasets <- load_AACT_dataset(AACT_folder, AACT_dataset_names)
 
 
 #----------------------------------------------------------------------------------------------------------------------
-# add manually checked dataset for German institutions
+# Load search terms for the affiliations/cities
 #----------------------------------------------------------------------------------------------------------------------
 
-german_lead_trials <- read_csv("metrics/summary_results_prospective_registration/german_UMCs_lead_trials_manually_checked.csv")
+city_search_terms <- c("Charite", "Charité")
+city_search_terms <-  paste0("\\b", city_search_terms, "\\b", collapse = "|")
 
-split_tibble <- function(tibble, col = 'col') tibble %>% split(., .[,col])
-german_lead_trials_ncts <- split_tibble(german_lead_trials, "city") %>% map(2)
+#----------------------------------------------------------------------------------------------------------------------
+#  search for studies with primary sponsor affiliations matching the search terms
+#----------------------------------------------------------------------------------------------------------------------
 
-charite_trials <- german_lead_trials_ncts$Berlin
+grep_fast <- function(pattern, x)
+{
+  return(which(str_detect(x, pattern)))
+}
+
+get_nct <- function(affil_indices, dataset)
+{
+  ncts <- dataset %>%
+    slice(affil_indices) %>%
+    select(nct_id)
+  return(ncts[[1]])
+}
+
+city_grep <- function(dataset, colname, grep_terms)
+{
+  indices <- map(tolower(grep_terms), grep_fast, x=tolower(dataset[[colname]]))
+  city_ncts <- map(indices, get_nct, dataset=dataset)
+  names(city_ncts) <- names(grep_terms)
+
+  return(city_ncts)
+}
+
+#joining of the different grep results
+affil_join <- function(affil_nct_list)
+{
+  affil_indices_joined <- affil_nct_list %>%
+    pmap(c) %>%
+    map(unique) %>%
+    map(sort)
+}
+
+#lead sponsor if affil found in fields (PI/sponsor/responsible_party)
+search_NCTS_lead_sponsor <- function(AACT_datasets, city_search_terms)
+{
+  #search the different affilation datasets for the city search terms
+  grep_PI <- city_grep(AACT_datasets$overall_officials, "affiliation", city_search_terms)
+  grep_sponsor <- city_grep(AACT_datasets$sponsors %>% filter(lead_or_collaborator == "lead"),
+                            "name", city_search_terms)
+  grep_resp_party_org <- city_grep(AACT_datasets$responsible_parties, "organization", city_search_terms)
+  grep_resp_party_affil <- city_grep(AACT_datasets$responsible_parties, "affiliation", city_search_terms)
+
+  #combine the results for the different fields
+  grep_results_primary <- list(grep_PI, grep_sponsor, grep_resp_party_org, grep_resp_party_affil)
+  affil_ncts_primary <- affil_join(grep_results_primary)
+
+  return(affil_ncts_primary)
+}
+
+institutions_ncts_primary <- search_NCTS_lead_sponsor(AACT_datasets, city_search_terms)[[1]]
 
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -63,13 +113,13 @@ CTgov_sample_full <- CTgov_sample_full %>%
 #now filter the trials for each institution into separate tables to summarize the results
 CTgov_sample_Charite <- list()
 CTgov_sample_Charite[[1]] <- CTgov_sample_full %>%
-  filter(nct_id %in% charite_trials)
+  filter(nct_id %in% institutions_ncts_primary)
 
 
 #results for the prospective registration metric
 summary_prosp_reg <- CTgov_sample_Charite %>%
   map(filter, start_date > "2006-01-01") %>%
-  map(filter, start_date < "2019-11-29") %>%
+  map(filter, start_date < "2019-12-31") %>%
   map(function(x) table(year(x[["start_date"]]), x[["has_prospective_registration"]])) %>%
   map(function(x) tibble(year = rownames(x), no_prosp_reg = x[,1],
                          has_prosp_reg = x[,2], perc_prosp_reg = x[,2]/rowSums(x)))
@@ -82,7 +132,7 @@ write_csv(summary_table_prosp_reg, "results/prospective_registration.csv")
 #results for the summary results registration metric
 summary_sum_res_12 <- CTgov_sample_Charite %>%
   map(filter, completion_date > "2006-01-01") %>%
-  map(filter, completion_date < "2018-11-29") %>%
+  map(filter, completion_date < "2018-12-31") %>%
   map(filter, study_type == "Interventional") %>%
   map(filter, overall_status %in% c("Completed", "Unknown status", "Terminated", "Suspended")) %>%
   map(function(x) table(year(x[["completion_date"]]), x[["summary_result_12_month"]])) %>%
@@ -97,7 +147,7 @@ write_csv(summary_table_sum_res_12, "results/summary_results_12_month.csv")
 #results for the summary results registration metric
 summary_sum_res_24 <- CTgov_sample_Charite %>%
   map(filter, completion_date > "2006-01-01") %>%
-  map(filter, completion_date < "2017-11-29") %>%
+  map(filter, completion_date < "2017-12-31") %>%
   map(filter, study_type == "Interventional") %>%
   map(filter, overall_status %in% c("Completed", "Unknown status", "Terminated", "Suspended")) %>%
   map(function(x) table(year(x[["completion_date"]]), x[["summary_result_24_month"]])) %>%
