bb315c604dbf4f3dc25fa8d69dcc6db6f28d47c5
Nico RIedel <nico.riedel@bihealth.de>
2021-01-04 17:04:43 +0100

    Minor update for contributors text

--- shiny_app/about_page.R
+++ shiny_app/about_page.R
@@ -1,23 +1,24 @@
-about_page <- tabPanel("About",
-
+about_page <- tabPanel("About", value = "tabAbout",
                        h3("Contributors"),
                        br(),
                        h4("ODDPub - Open Data & Code detection"),
                        helpText('Riedel, Nico (Conceptualization, Methodology, Technical Implementation, Validation);
-                     Bobrov, Evgeny (Conceptualization, Methodology, Validation); Kip, Miriam (Conceptualization, Methodology)'),
+                                Bobrov, Evgeny (Conceptualization, Methodology, Validation); Kip, Miriam (Conceptualization, Methodology)'),
                        br(),
                        h4("Barzooka - Vizualization type detection"),
                        helpText('Riedel, Nico (Conceptualization, Methodology, technical implementation);
-                     Weissgerber, Tracey (Conceptualization, Methodology); Schultz, Robert (Validation)'),
+                                Weissgerber, Tracey (Conceptualization, Methodology); Schultz, Robert (Validation)'),
                        br(),
                        h4("Clinical trial metrics"),
                        helpText('Riedel, Nico (Conceptualization, Methodology, technical implementation);
-                     Strech, Dainel; Wieschowski, Susanne; Grabitz, Peter; Franzen, Delwen; Salholz-Hillel, Maia'),
+                                Strech, Dainel (Conceptualization, Methodology); Wieschowski, Susanne (Conceptualization, Methodology);
+                                Grabitz, Peter (Conceptualization, Methodology); Franzen, Delwen (Conceptualization, Methodology);
+                                Salholz-Hillel, Maia (Conceptualization, Methodology)'),
                        br(),
                        h4("Shiny app"),
                        helpText('Riedel, Nico (Conceptualization, Technical Implementation); Weissgerber, Tracey (Conceptualization);
-                     Dirnagl, Ulrich (Conceptualization); Bobrov, Evgeny (Conceptualization); Strech, Daniel (Conceptualization);
-                     Franzen, Delwen (Conceptualization); Salholz-Hillel, Maia (Conceptualization)'),
+                                Dirnagl, Ulrich (Conceptualization); Bobrov, Evgeny (Conceptualization); Strech, Daniel (Conceptualization);
+                                Franzen, Delwen (Conceptualization); Salholz-Hillel, Maia (Conceptualization)'),
                        br(),
                        h3('Contact address'),
                        helpText('QUEST Center for Transforming Biomedical Research,'),
@@ -26,5 +27,5 @@ about_page <- tabPanel("About",
                        helpText('10178 Berlin '),
                        helpText('quest@bihealth.de'),
                        helpText(HTML('<a href="https://www.bihealth.org/quest-center/">
-                  https://www.bihealth.org/quest-center/ </a>'))
+                                      https://www.bihealth.org/quest-center/ </a>'))
 )
