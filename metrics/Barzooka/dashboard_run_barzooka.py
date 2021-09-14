614aa876b6cb2ac4f51d1dd19d14eca0a599baac
Nico <nico.riedel@bihealth.de>
2021-09-14 10:57:38 +0200

    Update barzooka scripts
    Now requires already installed barzooka python package

--- metrics/Barzooka/dashboard_run_barzooka.py
+++ metrics/Barzooka/dashboard_run_barzooka.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
-from fastai.vision.all import *
-from matplotlib import pyplot
-import numpy as np
-import pandas as pd
-np.set_printoptions(threshold=sys.maxsize)
+#first, need to install barzooka package from https://github.com/quest-bih/barzooka
+import barzooka
 
 #---------------------------------------------------------------------------------
 # run configs
 #---------------------------------------------------------------------------------
 
-#import barzooka_v2 or barzooka_v2_no_flow or barzooka_v2_flowyesno
-import barzooka_v2_flowyesno as barzooka_v2
+#tmp folder for img extraction from PDFs
 tmp_folder_run = './tmp/' 
  
+#might need to adjust PDF folder path according to PDF location
+pdf_folder = '/fast/users/riedeln_c/work/charite_dashboard/PDFs/' 
+
 #---------------------------------------------------------------------------------
 # calculate charite PDF validation
 #---------------------------------------------------------------------------------
 
-b = barzooka_v2.Barzooka('/fast/users/riedeln_c/work/charite_dashboard/barzooka_v2_flow.pkl')
-b.predict_from_folder('/fast/users/riedeln_c/work/charite_dashboard/PDFs_new/',
-        './results/dashboard_PDFs_new.csv', tmp_folder=tmp_folder_run)
+b = barzooka.Barzooka()
+
+b.predict_from_folder(pdf_folder,
+        './results/Barzooka.csv', tmp_folder=tmp_folder_run)
