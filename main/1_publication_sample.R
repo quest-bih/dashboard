e8f74c58af51c01c44c7dac646877d5f2c32dcd8
Nico <nico.riedel@bihealth.de>
2021-09-15 14:15:39 +0200

    Deal with missing dois in dataset

--- main/1_publication_sample.R
+++ main/1_publication_sample.R
@@ -17,11 +17,13 @@ publications_16_17 <- read_csv("./main/publication_table_library_2016_17.csv") %
          document_type = `Doc.Type`) %>%
   mutate(oa_indicator = NA,
          oa_status = NA,
+         doi = ifelse(doi == "0", NA, doi),
          database = database %>% str_remove(":[:digit:]{15}")) %>%
   select(colnames(publications_18_20)) %>%
   filter(!(doi %in% publications_18_20$doi),
          year != 0)
 
-publications <- rbind(publications_16_17, publications_18_20)
+publications <- rbind(publications_16_17, publications_18_20) %>%
+  arrange(doi)
 
 publications %>% write_csv("./main/publication_table_library_2016_20.csv")
